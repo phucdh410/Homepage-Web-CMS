019c59a5bd255f3528f2d560eaf5c96f8bb6b9f2
phucdh410 <phucdh410@gmail.com>
2023-04-07 17:55:02 +0700

    update

--- src/modules/homepage/components/MLinksForm/MLinksForm.tsx
+++ src/modules/homepage/components/MLinksForm/MLinksForm.tsx
@@ -1,7 +1,7 @@
 import { Controller, useForm } from 'react-hook-form';
-import { Box, FormLabel } from '@mui/material';
+import { Box, FormLabel, Stack } from '@mui/material';
 
-import { CInput } from '@/controls/';
+import { CActionsForm, CInput } from '@/controls/';
 import { ILinks } from '@/types/homepage-link';
 
 import { IMLinksFormProps } from './types';
@@ -23,30 +23,95 @@ export const MLinksForm: React.FC<IMLinksFormProps> = ({ data }) => {
   //#endregion
 
   //#region Event
+  const onSubmit = () => {
+    handleSubmit(async (values) => {
+      console.log(values);
+    })();
+  };
 
+  const onCancel = () => {
+    reset();
+  };
   //#endregion
 
   //#region Render
   return (
     <Box>
-      <FormLabel title="Liên kết" required sx={{ mb: 3 }} />
+      <form onSubmit={onSubmit}>
+        <Box mb={2}>
+          <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+            Liên kết
+          </FormLabel>
+        </Box>
+        <Box px={2}>
+          <Stack direction="column" spacing={1} mb={2.5}>
+            <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+              Youth HCMUE
+            </FormLabel>
+            <Controller
+              control={control}
+              name="youth"
+              render={({ field }) => (
+                <CInput {...field} placeholder="Nhập liên kết" />
+              )}
+            />
+          </Stack>
 
-      <Box px={3}>
-        <Controller
-          control={control}
-          name="youth"
-          render={({ field }) => (
-            <CInput {...field} placeholder="Nhập liên kết" />
-          )}
-        />
-        <Controller
-          control={control}
-          name="online"
-          render={({ field }) => (
-            <CInput {...field} placeholder="Nhập liên kết" />
-          )}
-        />
-      </Box>
+          <Stack direction="column" spacing={1} mb={2.5}>
+            <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+              Online Sinh viên
+            </FormLabel>
+            <Controller
+              control={control}
+              name="online"
+              render={({ field }) => (
+                <CInput {...field} placeholder="Nhập liên kết" />
+              )}
+            />
+          </Stack>
+
+          <Stack direction="column" spacing={1} mb={2.5}>
+            <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+              Đăng ký giấy chứng nhận
+            </FormLabel>
+            <Controller
+              control={control}
+              name="certificate"
+              render={({ field }) => (
+                <CInput {...field} placeholder="Nhập liên kết" />
+              )}
+            />
+          </Stack>
+
+          <Stack direction="column" spacing={1} mb={2.5}>
+            <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+              Hỗ trợ sinh viên
+            </FormLabel>
+            <Controller
+              control={control}
+              name="help"
+              render={({ field }) => (
+                <CInput {...field} placeholder="Nhập liên kết" />
+              )}
+            />
+          </Stack>
+
+          <Stack direction="column" spacing={1} mb={2.5}>
+            <FormLabel sx={{ fontWeight: 600, lineHeight: '24px' }} required>
+              Facebook
+            </FormLabel>
+            <Controller
+              control={control}
+              name="facebook"
+              render={({ field }) => (
+                <CInput {...field} placeholder="Nhập liên kết" />
+              )}
+            />
+          </Stack>
+
+          <CActionsForm onCancel={onCancel} onSubmit={onSubmit} />
+        </Box>
+      </form>
     </Box>
   );
   //#endregion
